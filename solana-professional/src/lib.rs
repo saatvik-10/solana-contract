df2929570338b694028a036bacde20942cc6f5a5
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-26 16:22:42 +0530

    updated lib for counters

--- solana-professional/src/lib.rs
+++ solana-professional/src/lib.rs
@@ -1,5 +1,7 @@
+mod instruction;
 mod state;
 use borsh::{BorshDeserialize, BorshSerialize};
+use instruction::CounterInstruction;
 use solana_program::entrypoint;
 use solana_program::entrypoint::ProgramResult;
 use solana_program::{
@@ -19,29 +21,69 @@ pub fn process_instruction(
     accounts: &[AccountInfo], //accounts involved in transaction
     instruction_data: &[u8],  //data passed by caller to program
 ) -> ProgramResult {
-    msg!("Increment Counter");
+    // msg!("Increment Counter");
 
+    // let account_iter = &mut accounts.iter(); //getting the account iterator
+    // let counter_account = next_account_info(account_iter)?; // The first account is the one where we store the counter
+    // let payer = next_account_info(account_iter)?; // The second account is the "payer" (who pays for the operation)
+    // let system_program = next_account_info(account_iter)?; // The third account is the system program
+
+    // //counter account is owned by our program
+    // if counter_account.owner != program_id {
+    //     msg!("Counter account is not the owner");
+    //     return Err(ProgramError::IncorrectProgramId);
+    // }
+
+    // //Deserialize the Counter from the account data
+    // let mut counter = Counter::try_from_slice(&counter_account.data.borrow())?;
+    // msg!("Current Counter: {}", counter.value);
+
+    // //Increment the counter
+    // counter.value += 1;
+    // msg!("New counter: {}", counter.value);
+
+    // //Serialize the Counter back to the account data
+    // counter.serialize(&mut &mut counter_account.data.borrow_mut()[..])?;
+
+    // Ok(())
+
+    let instruction = CounterInstruction::try_from_slice(instruction_data)?; //decodes the instruction bytes
     let account_iter = &mut accounts.iter(); //getting the account iterator
     let counter_account = next_account_info(account_iter)?; // The first account is the one where we store the counter
-    let payer = next_account_info(account_iter)?; // The second account is the "payer" (who pays for the operation)
-    let system_program = next_account_info(account_iter)?; // The third account is the system program
 
-    //counter account is owned by our program
-    if counter_account.owner != program_id {
-        msg!("Counter account is not the owner");
-        return Err(ProgramError::IncorrectProgramId);
+    match instruction {
+        CounterInstruction::Initialize => {
+            msg!("Instruction: Initialize");
+
+            //checking ownership
+            if counter_account.owner != program_id {
+                msg!("Counter account is now owned by the program");
+                return Err(ProgramError::IncorrectProgramId);
+            }
+
+            //initializing the counter with zero
+            let counter = Counter { value: 0 };
+            counter.serialize(&mut &mut counter_account.data.borrow_mut()[..])?; //provide a mutable reference (&mut) to that mutable buffer so the serializer can write into it
+
+            msg!("Counter account has been initialized with value 0");
+        }
+
+        CounterInstruction::Increment => {
+            msg!("Instruction: Increment");
+
+            if counter_account.owner != program_id {
+                msg!("Counter account not owned by the program");
+                return Err(ProgramError::IncorrectProgramId);
+            }
+
+            let mut counter = Counter::try_from_slice(&counter_account.data.borrow())?;
+            msg!("Current Counter: {}", counter.value);
+
+            counter.value += 1;
+            msg!("New Counter: {}", counter.value);
+
+            counter.serialize(&mut &mut counter_account.data.borrow_mut()[..])?;
+        }
     }
-
-    //Deserialize the Counter from the account data
-    let mut counter = Counter::try_from_slice(&counter_account.data.borrow())?;
-    msg!("Current Counter: {}", counter.value);
-
-    //Increment the counter
-    counter.value += 1;
-    msg!("New counter: {}", counter.value);
-
-    //Serialize the Counter back to the account data
-    counter.serialize(&mut &mut counter_account.data.borrow_mut()[..])?;
-
     Ok(())
 }
