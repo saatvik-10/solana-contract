37eed7130b98b80f12c4b91dd8963e2abc6e67cd
saatvik-10 <saatvikmadan529@gmail.com>
2025-07-29 01:01:21 +0530

    created constants

---
+++ solana-client/src/constants.ts
@@ -0,0 +1,6 @@
+//seed used for generating the PDA
+//should exactly match the one used in the Rust program
+export const COUNTER_SEED = 'counter';
+
+//the on-chain program ID of your deployed Solana program
+export const PROGRAM_ID = process.env.PROGRAM_ID;
